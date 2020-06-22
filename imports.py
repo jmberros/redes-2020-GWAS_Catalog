9d390631db72417ea667a3801529e40491e59c70
Juan Manuel Berros <juanmaberros@gmail.com>
2020-06-21 22:10:04 -0300

    Emprolija el EDA. Comienza OR vs betas.

---
+++ imports.py
@@ -0,0 +1,24 @@
+from os import makedirs
+from operator import itemgetter
+
+from tqdm import tqdm
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import networkx as nx
+import igraph
+
+pd.set_option("max_columns", 100)
+pd.set_option("colwidth", 400)
+
+sns.set(context="notebook", style="darkgrid")
+
+data_dir = "data"
+results_dir = "results"
+
+makedirs(results_dir, exist_ok=True)
+
+tqdm.pandas() # Habilita usar df.progress_apply
